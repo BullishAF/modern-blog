f5bd65d75c7e9da6acc81606cbff1851eaa4d5e9
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-02-28 23:36:18 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ThreeIC Blog';
-export const SITE_DESCRIPTION = 'Thoughts about crypto and other topics';
+export const SITE_TITLE = '3IC Blog';
+export const SITE_DESCRIPTION = 'Thoughts about crypto, defi and other topics';

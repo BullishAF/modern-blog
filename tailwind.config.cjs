1d813eb731120a36928613ee1cb2a8a3fc8e49ea
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-04-01 22:31:12 -0400

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
+	 mytheme: {
+          
+ "primary": "#22d3ee",
+          
+ "secondary": "#22d3ee",
+          
+ "accent": "#9333ea",
+          
+ "neutral": "#1E293B",
+          
+ "base-100": "#0F172A",
+          
+ "info": "#0CA5E9",
+          
+ "success": "#2DD4BF",
+          
+ "warning": "#F4BF50",
+          
+ "error": "#FB7085",
+          },
+        },
+      ],
+    },
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 }
